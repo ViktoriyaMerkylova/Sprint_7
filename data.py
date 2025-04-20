8ea95f1a15f0b42798b36d8b8a0a82475808306f
Viktoriya Merkylova <v.valkiriya1@yandex.ru>
2025-04-20 14:29:53 +0300

    Creation requirements.txt

--- data.py
+++ data.py
@@ -7,3 +7,57 @@ class Url:
 
 login_valid = 'valkiriya'
 password_valid = '3333333!'
+
+order_black = {
+    "firstName": "John",
+    "lastName": "Snow",
+    "address": "the Wall",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-08-06",
+    "comment": "I don't know nothing",
+    "color": [
+        "BLACK"
+    ]
+}
+
+order_grey = {
+    "firstName": "Daenerys",
+    "lastName": "Targaryen",
+    "address": "Antient Valyria",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 00",
+    "rentTime": 5,
+    "deliveryDate": "2024-08-06",
+    "comment": "Drakaris",
+    "color": [
+        "GREY"
+    ]
+}
+
+order_black_grey = {
+    "firstName": "Tyrion",
+    "lastName": "Lannister",
+    "address": "Caterly Rock",
+    "metroStation": 3,
+    "phone": "+7 800 355 0988",
+    "rentTime": 1,
+    "deliveryDate": "2024-08-06",
+    "comment": "I talk too much",
+    "color": [
+        "GREY", "BLACK"
+    ]
+}
+
+order_no_color = {
+    "firstName": "Sansa",
+    "lastName": "Stark",
+    "address": "Winterfell",
+    "metroStation": 3,
+    "phone": "+7 800 445 0988",
+    "rentTime": 4,
+    "deliveryDate": "2024-08-06",
+    "comment": "I like dogs",
+    "color": []
+}
